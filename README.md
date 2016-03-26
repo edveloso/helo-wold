84f0d4bb088bc8ff1c3a7fe363d9bfe4f3c9bfd5
edveloso <edveloso@gmail.com>
2016-03-26 09:35:04 -0300

    fist release temptatve

--- README.md
+++
@@ -1,3 +0,0 @@
-# helo-wold
-
-Editing in another branch
---
+++ src/main/java/br/com/app2haus/App.java
@@ -0,0 +1,13 @@
+package br.com.app2haus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
